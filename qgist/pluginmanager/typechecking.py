47e7ed48a4c91b832bd82aba1f20206508951d5e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-03-18 17:16:22 +0100

    type checking

---
+++ qgist/pluginmanager/typechecking.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST PLUGIN MANAGER
+QGIS Plugin for Managing QGIS Plugins
+https://github.com/qgist/pluginmanager
+
+    qgist/pluginmanager/typechecking.py: (Duck) typ checks
+
+    Copyright (C) 2017-2020 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/pluginmanager/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def is_QgsSettings(obj):
+
+    obj_type = type(obj)
+
+    if not hasattr(obj_type, '__name__'):
+        return False
+
+    if obj_type.__name__ != 'QgsSettings':
+        return False
+
+    if not hasattr(obj, 'setValue') or not hasattr(obj, 'value'):
+        return False
+
+    return True
