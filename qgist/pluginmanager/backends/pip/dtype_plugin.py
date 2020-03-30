351084d232ef34818aec9f9b3de5c7ccf5904c52
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-03-30 21:19:20 +0200

    license headers

---
+++ qgist/pluginmanager/backends/pip/dtype_plugin.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+QGIST PLUGIN MANAGER
+QGIS Plugin for Managing QGIS Plugins
+https://github.com/qgist/pluginmanager
+
+    qgist/pluginmanager/backends/pip/dtype_plugin.py: Plugin data type
+
+    Copyright (C) 2017-2020 QGIST project <info@qgist.org>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU General Public License
+Version 2 ("GPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+https://github.com/qgist/pluginmanager/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
