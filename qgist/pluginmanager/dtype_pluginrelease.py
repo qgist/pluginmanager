bb7cc32267b5c4399bc56a426b4b94d0cdff47c9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-04-01 22:16:50 +0200

    plugin release prototype

--- qgist/pluginmanager/dtype_pluginrelease.py
+++ qgist/pluginmanager/dtype_pluginrelease.py
@@ -6,7 +6,7 @@ QGIST PLUGIN MANAGER
 QGIS Plugin for Managing QGIS Plugins
 https://github.com/qgist/pluginmanager
 
-    qgist/pluginmanager/dtype_pluginversion.py: Plugin version data type
+    qgist/pluginmanager/dtype_pluginrelease.py: Plugin release data type
 
     Copyright (C) 2017-2020 QGIST project <info@qgist.org>
 
@@ -23,3 +23,78 @@ specific language governing rights and limitations under the License.
 </LICENSE_BLOCK>
 
 """
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT (Internal)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from .dtype_metadata import dtype_metadata_class
+from .dtype_version import dtype_version_class
+
+from ..error import (
+    QgistTypeError,
+    QgistValueError,
+    )
+from ..util import tr
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class dtype_plugin_release_class:
+    """
+    A release of a certain plugin
+
+    Mutable.
+    """
+
+    def __init__(self,
+        plugin_id, version,
+        has_processingprovider, has_serverfuncs, experimental,
+        meta):
+
+        if not isinstance(plugin_id, str):
+            raise QgistTypeError(tr('"plugin_id" must be a str.'))
+        if len(plugin_id) == 0:
+            raise QgistValueError(tr('"plugin_id" must not be empty.'))
+        if not isinstance(version, dtype_version_class):
+            raise QgistTypeError(tr('"version" must be a version.'))
+        if not isinstance(has_processingprovider, bool):
+            raise QgistTypeError(tr('"has_processingprovider" must be a bool.'))
+        if not isinstance(has_serverfuncs, bool):
+            raise QgistTypeError(tr('"has_serverfuncs" must be a bool.'))
+        if not isinstance(experimental, bool):
+            raise QgistTypeError(tr('"experimental" must be a bool.'))
+        if not isinstance(meta, dtype_metadata_class):
+            raise QgistTypeError(tr('"meta" must be meta data.'))
+
+        self._id = plugin_id
+        self._version = version
+        self._has_processingprovider = has_processingprovider
+        self._has_serverfuncs = has_serverfuncs
+        self._experimental = experimental
+        self._meta = meta
+
+    def __repr__(self):
+
+        return (
+            '<plugin_version '
+            f'id="{self._id:s}" name="{self._name:s}" '
+            f'installed={"yes" if self._installed else "no":s} '
+            f'active={"yes" if self._active else "no":s} '
+            f'deprecated={"yes" if self._deprecated else "no":s} '
+            f'protected={"yes" if self._protected else "no":s}'
+            '>'
+            )
+
+    def __eq__(self):
+
+        pass
+
+    @property
+    def has_processingprovider(self):
+        return self._has_processingprovider
+
+    @property
+    def version(self):
+        return self._version
