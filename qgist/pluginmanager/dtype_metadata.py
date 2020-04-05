fd0a36adce2ff1b985f7dbb09b032596bb4ebbf5
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-04-05 10:55:46 +0200

    re-design of meta data field

--- qgist/pluginmanager/dtype_metadata.py
+++ qgist/pluginmanager/dtype_metadata.py
@@ -35,7 +35,7 @@ from collections import OrderedDict
 # IMPORT (Internal)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-from .const import (
+from .metadata_spec import (
     METADATA_FIELD_DTYPES,
     METADATA_FIELDS_SPEC,
     )
@@ -61,7 +61,7 @@ class dtype_metadata_class:
     """
     Meta data of one single plugin
 
-    Immutable.
+    Mutable.
     """
 
     def __init__(self, **fields):
@@ -159,12 +159,13 @@ class _dtype_metadata_field_class:
     """
     Represents one field of meta data
 
-    Mutable, but not exposed beyond dtype_metadata_class, which is immutable.
+    Mutable.
     """
 
     def __init__(self,
         name, dtype,
-        is_required = False, value = None, i18n = False, known = True,
+        value = None, default_value = None, importer = None, exporter = None,
+        is_required = False, i18n = False, known = True,
         comment = '',
         ):
 
@@ -172,12 +173,14 @@ class _dtype_metadata_field_class:
             raise QgistTypeError(tr('"name" must be a str.'))
         if len(name) == 0:
             raise QgistValueError(tr('"name" must not be empty.'))
-        if not self._check_dtype(dtype):
+        if dtype not in METADATA_FIELD_DTYPES:
             raise QgistTypeError(tr('"dtype" unknown or broken.'))
+        if not hasattr(importer, '__call__') and importer is not None:
+            raise QgistTypeError(tr('"importer" must be callable or None.'))
+        if not hasattr(exporter, '__call__') and exporter is not None:
+            raise QgistTypeError(tr('"exporter" must be callable or None.'))
         if not isinstance(is_required, bool):
             raise QgistTypeError(tr('"is_required" must be a bool.'))
-        if not isinstance(value, dtype) and value is not None:
-            raise QgistTypeError(tr('"value" does not have matching type.'))
         if not isinstance(i18n, bool):
             raise QgistTypeError(tr('"i18n" must be a bool.'))
         if not isinstance(known, bool):
@@ -187,77 +190,97 @@ class _dtype_metadata_field_class:
 
         self._name = name
         self._dtype = dtype
+        self._importer = importer
+        self._exporter = exporter
         self._is_required = is_required
-        self._value = value
-        self._i18n = i18n # TODO unused
+        self._value = None
         self._known = known # is meta field a known one?
+
+        self._i18n = i18n # TODO unused
         self._comment = comment # TODO unused
+
+        if not self._is_valid_value(value) and value is not None:
+            raise QgistTypeError(tr('"value" does not have matching tyspe.'))
+        if not self._is_valid_value(default_value) and default_value is not None:
+            raise QgistTypeError(tr('"default_value" does not have matching tyspe.'))
+
+        self._value = value
+        self._default_value = default_value
 
     def __repr__(self):
 
         return (
             '<meta_field '
             f'name="{self._name:s}" '
-            f'dtype={self._dtype.__name__ if not isinstance(self._dtype, dict) else str(self._dtype):s} '
+            f'dtype={getattr(self._dtype, "__name__", str(self._dtype)):s} '
+            f'set={"yes" if self.value_set else "no"} '
             f'known={"yes" if self._known else "no"} '
             f'i18n={"yes" if self._i18n else "no"} '
             f'required={"yes" if self._is_required else "no"}'
             '>'
             )
 
+    def _is_valid_value(self, value):
+
+        return isinstance(value, self._dtype)
+
+    def _value_to_string(self, value):
+
+        if self._exporter is None:
+            return str(value)
+
+        value_str = self._exporter(value)
+        if not isinstance(value_str, str):
+            raise QgistTypeError(tr('"value_str" must be a str.'))
+        return value_str
+
+    @property
+    def value_set(self):
+        return self._value is not None
+
+    @property
+    def default_value_set(self):
+        return self._default_value is not None
+
     @property
     def value(self):
-
         return self._value
 
     @value.setter
     def value(self, new_value):
-
-        if (
-            not any((isinstance(new_value, dtype) for dtype in METADATA_FIELD_DTYPES))
-            or
-            (isinstance(new_value, str) and isinstance(self._dtype, dict))
-            ):
+        if not self._is_valid_value(new_value):
             raise QgistTypeError(tr('"new_value" does not have valid type'))
-
-        if self._dtype == str:
-            new_value = str(new_value)
-        if self._dtype == bool:
-            new_value = dtype_settings_class.str_to_bool(new_value)
-        if isinstance(self._dtype, dict):
-            if self._dtype['name'] == 'dtype_version_class':
-                new_value = getattr(dtype_version_class, f'from_{self._dtype["method"]:s}')
-
-        if not isinstance(new_value, self._dtype) and not isinstance(new_value, dtype_version_class):
-            raise QgistTypeError(tr('"new_value" was not converted to correct type'))
-
         self._value = new_value
 
     @property
+    def default_value(self):
+        return self._default_value
+
+    @property
+    def value_string(self):
+        if not self.value_set:
+            raise QgistValueError(tr('Nothing to export to string - value not set.'))
+        return self._value_to_string(self._value)
+
+    @value_string.setter
+    def value_string(self, new_value_str):
+        if not isinstance(new_value_str, str):
+            raise QgistTypeError(tr('"new_value_str" must be a str.'))
+        if self._importer is not None:
+            new_value = self._importer(new_value_str)
+        else:
+            new_value = self._dtype(new_value_str)
+        self.value = new_value
+
+    @property
+    def default_value_string(self):
+        if not self.default_value_set:
+            raise QgistValueError(tr('Nothing to export to string - default_value not set.'))
+        return self._value_to_string(self._default_value)
+
+    @property
     def is_required(self):
-
         return self._is_required
-
-    @staticmethod
-    def _check_dtype(dtype):
-
-        if dtype in METADATA_FIELD_DTYPES:
-            return True
-
-        if not isinstance(dtype, dict):
-            return False
-        if dtype.keys() != {'name', 'method', 'kwargs'}:
-            return False
-        if dtype['name'] != 'dtype_version_class':
-            return False
-        if not isinstance(dtype['method'], str) and isinstance(dtype['kwargs'], dict):
-            return False
-        if not hasattr(dtype_version_class, f'from_{dtype["method"]:s}'):
-            return False
-        if not all((isinstance(key, str) for key in dtype['kwargs'].keys())):
-            return False
-
-        return True
 
     @classmethod
     def from_unknown(cls, name, value):
